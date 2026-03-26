6e41d3e6abc57208346f9221ab1da38871c59fbe
Pavlo Sharipov <pavel.sharipov@gmail.com>
2026-03-26 10:14:47 +0100

    init clean spring boot project skeleton

---
+++ src/main/java/de/psharipov/deviceinventory/DeviceInventoryApiApplication.java
@@ -0,0 +1,13 @@
+package de.psharipov.deviceinventory;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DeviceInventoryApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DeviceInventoryApiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/de/psharipov/deviceinventory/DeviceInventoryApiApplicationTests.java
@@ -0,0 +1,13 @@
+package de.psharipov.deviceinventory;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DeviceInventoryApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
